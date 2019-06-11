11334fa43b1c426bf9049396ee6c84be45185e70
Darcy Linde <lindedarcy@gmail.com>
2019-06-11 14:33:20 -0400

    added ingredients routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :show]
+      resources :ingredients, only: [:create, :show]
       post '/login', to: 'auth#create', as: 'login'
     end
   end
