2d18575f67f147df8143dba102b1347ae73625b8
Darcy Linde <lindedarcy@gmail.com>
2019-06-14 10:51:33 -0400

    made template for recipes_controller

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::RecipesController < ApplicationController
+    #before_action :authorize!, only: [:create]
+    
+    def index
+      @recipes = Recipe.all
+      render json: @recipes, status: :ok
+    end
+
+    def show
+      @recipe = Recipe.find(params[:id])
+      render json: @recipe, status: :ok
+    end
+  
+    def create
+      @recipe = Recipe.find_by(recipe_params)
+      if @recipe
+        render json: @recipe, status: :created
+      else
+        @new_recipe = Recipe.new(recipe_params)
+        if @new_recipe.save
+          render json: @new_recipe, status: :created
+        else
+          render json: { errors: @new_recipe.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    end
+  
+    private
+    def recipe_params
+      params.permit(
+          :api_id, 
+          :title, 
+          :image, 
+          :instructions, 
+          :servings, 
+          :preparation_minutes, 
+          :cooking_minutes, 
+          :ready_in_minutes, 
+          :price_per_serving, 
+          :source_url, 
+          :vegetarian, 
+          :vegan, 
+          :gluten_free, 
+          :dairy_free)
+    end
+  end
